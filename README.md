25ab5daedfd182813fdeb0535e5a22762fa14475
Jamison Ordway <jamisonordway@gmail.com>
2021-09-22 10:10:01 -0600

    Creates tables: mods, instructors, students, dates, attendances

--- README.md
+++ README.md
@@ -1,24 +1,6 @@
-# README
+# Turing School Zoom Attendance
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+work in progress!
 
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+DB Visual Schema:
+![visual schema](app/assets/images/visual_schema.png)
---
+++ db/schema.rb
@@ -0,0 +1,60 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_09_22_160332) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "attendances", force: :cascade do |t|
+    t.string "email"
+    t.bigint "date_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["date_id"], name: "index_attendances_on_date_id"
+  end
+
+  create_table "dates", force: :cascade do |t|
+    t.integer "time_of_day"
+    t.string "status"
+    t.bigint "mod_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mod_id"], name: "index_dates_on_mod_id"
+  end
+
+  create_table "instructors", force: :cascade do |t|
+    t.string "name"
+    t.bigint "mod_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mod_id"], name: "index_instructors_on_mod_id"
+  end
+
+  create_table "mods", force: :cascade do |t|
+    t.string "name"
+    t.integer "program"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "attendances", "dates"
+  add_foreign_key "dates", "mods"
+  add_foreign_key "instructors", "mods"
+end
