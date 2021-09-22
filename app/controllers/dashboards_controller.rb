8833c1977e163a1431febb86a60dd40f6959319c
Dione <dionew1@gmail.com>
2021-09-22 13:55:26 -0600

    feat:WIP retrieve live meeting for dashboard index

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,8 @@
 class DashboardsController < ApplicationController
   def index
+    resp = Faraday.get("https://api.zoom.us/v2/metrics/meetings") do |req|
+      req.headers["Authorization"] = "Bearer #{params[:token]}"
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'login#show'
 
+  get '/login', to: 'login#create'
   resources :dashboards, only: [:index]
   resources :lesson_dates, only: [:index, :show]
 end
