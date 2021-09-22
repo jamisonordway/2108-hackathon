be5cbc422bf12e87f6125aaaa7958d1c3c91f524
Jamison Ordway <jamisonordway@gmail.com>
2021-09-22 10:33:51 -0600

    change 'dates' to 'lesson_dates'

--- app/controllers/dates_controller.rb
+++
@@ -1,9 +0,0 @@
-class DatesController < ApplicationController
-  def index
-    @dates = Date.all
-  end
-
-  def show
-    @date = Date.find(params[:id])
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get root, to: 'dashboards#index'
 
-  resources :dates, only: [:index, :show]
+  resources :lesson_dates, only: [:index, :show]
 end
